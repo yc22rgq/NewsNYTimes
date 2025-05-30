75e081d810bdc77327b690ccc623009258df7872
fixik <irinagomel10@gmail.com>
2025-05-30 13:24:28 +0300

    Initial Commit

---
+++ NewsNYTimes/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NewsNYTimes
+//
+//  Created by Эдуард Кудянов on 30.05.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NewsNYTimes/NewsNYTimesApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsNYTimesApp.swift
+//  NewsNYTimes
+//
+//  Created by Эдуард Кудянов on 30.05.25.
+//
+
+import SwiftUI
+
+@main
+struct NewsNYTimesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
